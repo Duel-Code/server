0b2a4db3f9b853bea1afd1c6462928267a7f2208
NiumXp <formigacomcaimbra@gmail.com>
2021-10-11 12:12:36 -0300

    adiciona enums e dataclasses

--- server/manager.py
+++ server/manager.py
@@ -1,15 +1,48 @@
 from __future__ import annotations
 
+import enum
 import typing
-from dataclasses import dataclass
+from datetime import datetime
+from dataclasses import dataclass, field
 
 if typing.TYPE_CHECKING:
     from .server import Server
 
+_ = enum.auto
+
+
+def random_game_id() -> str:
+    dt = datetime.now()
+    dt = dt.strftime("%H%M%S%f")
+    dt = int(dt)
+    return hex(dt)[2:]
+
+
+class Language(enum.IntEnum):
+    PYTHON = _()
+    JAVASCRIPT = _()
+
+
+class State(enum.IntEnum):
+    WAITING = _()
+    STARTING = _()
+    RUNNING = _()
+    FINISHED = _()
+    TIMEOUT = _()
+
+
+@dataclass
+class User:
+    name: str
+    language: Language
+    disconnected: bool = field(default=False)
+
 
 @dataclass
 class Game:
-    id_: str
+    id_: str = field(default_factory=random_game_id)
+    state: State = field(default=State.WAITING)
+    users: typing.List[User] = field(default_factory=list)
 
 
 class Manager:
